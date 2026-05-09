4ae1dd2f5b9129e2781cd7ff4588be8bc1140b41
Przemysław Klimont <przemyslaw.klimont@ttsw.com.pl>
2026-05-09 13:20:06 +0200

    Print shopping list

--- task1.py
+++ task1.py
@@ -1,6 +1,13 @@
 shopping_list = {"Piekarnia":["Chleb","Pączek","Bułki"],
                  "Warzywniak":["Marchew","Seler","Rukola"]}
 
+for key in shopping_list.keys():
+    products = [v.upper() for v in shopping_list[key]]
+    print(f"Idę do {key.upper()}, kupuję następujące rzeczy: {products}")
+
+
+
+
 
 
 
