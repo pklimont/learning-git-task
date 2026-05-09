8d3b932f6d7c6dec37e910c349cc2926339ee449
Przemysław Klimont <przemyslaw.klimont@ttsw.com.pl>
2026-05-09 13:20:26 +0200

    Print summary

--- task1.py
+++ task1.py
@@ -5,6 +5,7 @@ for key in shopping_list.keys():
     products = [v.upper() for v in shopping_list[key]]
     print(f"Idę do {key.upper()}, kupuję następujące rzeczy: {products}")
 
+print(f"W sumię kupuję {sum(len(v) for v in shopping_list.values())} produktów")
 
 
 
