b23d419dd468134701d1609293d1c276c3d158f5
Przemysław Klimont <przemyslaw.klimont@ttsw.com.pl>
2026-05-14 16:03:28 +0200

    Fix code task1

--- task1.py
+++ task1.py
@@ -1,9 +1,9 @@
-shopping_list = {"Piekarnia":["Chleb","Pączek","Bułki"],
-                 "Warzywniak":["Marchew","Seler","Rukola"]}
+shopping_list = {"piekarnia":["chleb","pączek","bułki"],
+                 "warzywniak":["marchew","seler","rukola"]}
 
 for key in shopping_list.keys():
-    products = [v.upper() for v in shopping_list[key]]
-    print(f"Idę do {key.upper()}, kupuję następujące rzeczy: {products}")
+    products = [v.capitalize() for v in shopping_list[key]]
+    print(f"Idę do {key.capitalize()}, kupuję następujące rzeczy: {products}")
 
 print(f"W sumię kupuję {sum(len(v) for v in shopping_list.values())} produktów")
 
